bb22e1f6991fe754bef971ca4bf656b56d87403b
Wonsun Ahn <wahn@wahn.cs.pitt.edu>
2020-01-30 15:51:31 -0500

    reorganized folders

--- exercises/2/Cat.java
+++ exercises/2/Cat.java
@@ -1,117 +1,117 @@
 public class Cat {
 
-    /**
-     * Indicates whether or not the cat is rented.
-     */
+	/**
+	 * Indicates whether or not the cat is rented.
+	 */
 
-    boolean _rented = false;
+	boolean _rented = false;
 
-    /**
-     * ID of the cat
-     * By default, -1
-     */
-    int _id = -1;
+	/**
+	 * ID of the cat By default, -1
+	 */
+	int _id = -1;
 
-    /**
-     * Name of the cat
-     */
+	/**
+	 * Name of the cat
+	 */
 
-    String _name;
+	String _name;
 
-    /**
-     * Whether bug injection is turned on for the Cat class.
-     * Turn off before actual usage.  Only leave on during JUnit testing.
-     */
+	/**
+	 * Whether bug injection is turned on for the Cat class. Turn off before actual
+	 * usage. Only leave on during JUnit testing.
+	 */
 
-    public static boolean _bugInjectionOn = true;
-    
-    /**
-     * Constructor - creates a new Cat object
-     * Note there are no checks that this ID is not taken by another
-     * cat!  This is probably something that we would fix in a production
-     * system.
-     * @param int id - the id number of this cat
-     * @param String name - the name of this Cat
-     */
-    
-    public Cat(int id, String name) {
-    	_rented = false;
-    	_id = id;
-    	_name = name;
-    }
+	public static boolean _bugInjectionOn = true;
 
-    /**
-     * Rent cat.  Simply sets the _rented flag to true.
-     */
+	/**
+	 * Constructor - creates a new Cat object Note there are no checks that this ID
+	 * is not taken by another cat! This is probably something that we would fix in
+	 * a production system.
+	 * 
+	 * @param int    id - the id number of this cat
+	 * @param String name - the name of this Cat
+	 */
 
-    public void rentCat() {
-    	if (_bugInjectionOn) {
-    		throw new UnsupportedOperationException("Injected bug for rentCat()");
-    	}
-    	_rented = true;
-    }
+	public Cat(int id, String name) {
+		_rented = false;
+		_id = id;
+		_name = name;
+	}
 
-    /**
-     * Return cat.  Simply sets the _rented flag to false.
-     */
+	/**
+	 * Rent cat. Simply sets the _rented flag to true.
+	 */
 
-    public void returnCat() {
-    	if (_bugInjectionOn) {
-    		throw new UnsupportedOperationException("Injected bug for returnCat()");
-    	}
-    	_rented = false;
-    }
+	public void rentCat() {
+		if (_bugInjectionOn) {
+			throw new UnsupportedOperationException("Injected bug for rentCat()");
+		}
+		_rented = true;
+	}
 
-    /**
-     * Accessor for _name variable.  Returns the name of this cat.
-     * @return String name of cat
-     */
+	/**
+	 * Return cat. Simply sets the _rented flag to false.
+	 */
 
-    public String getName() {
-    	if (_bugInjectionOn) {
-    		throw new UnsupportedOperationException("Injected bug for getName()");
-    	}
-    	return _name;
-    }
+	public void returnCat() {
+		if (_bugInjectionOn) {
+			throw new UnsupportedOperationException("Injected bug for returnCat()");
+		}
+		_rented = false;
+	}
 
-    /**
-     * Accessor for _id variable.  Returns the ID of this cat.
-     * @return int ID of this cat
-     */
+	/**
+	 * Accessor for _name variable. Returns the name of this cat.
+	 * 
+	 * @return String name of cat
+	 */
 
-    public int getId() {
-    	if (_bugInjectionOn) {
-    		throw new UnsupportedOperationException("Injected bug for getId()");
-    	}
-    	return _id;
-    }
+	public String getName() {
+		if (_bugInjectionOn) {
+			throw new UnsupportedOperationException("Injected bug for getName()");
+		}
+		return _name;
+	}
 
-    /**
-     * Accessor for _rented variable.  Returns if cat is rented.
-     * @return boolean - true if rented, false otherwise
-     */
+	/**
+	 * Accessor for _id variable. Returns the ID of this cat.
+	 * 
+	 * @return int ID of this cat
+	 */
 
-    public boolean getRented() {
-    	if (_bugInjectionOn) {
-    		throw new UnsupportedOperationException("Injected bug for getRented()");
-    	}
-    	return _rented;
-    }
+	public int getId() {
+		if (_bugInjectionOn) {
+			throw new UnsupportedOperationException("Injected bug for getId()");
+		}
+		return _id;
+	}
 
-    /**
-     * Returns string version of this cat, in form: "ID *id_num*. *name*"
-     * Example for cat of ID 1, name Jennyanydots: "ID 1. Jennyanydots"
-     * @return String string version of cat
-     */
+	/**
+	 * Accessor for _rented variable. Returns if cat is rented.
+	 * 
+	 * @return boolean - true if rented, false otherwise
+	 */
 
-    public String toString() {
-    	if (_bugInjectionOn) {
-    		throw new UnsupportedOperationException("Injected bug for toString()");
-    	}
-    	return "ID " + _id + ". " + _name;
-    }
-    
+	public boolean getRented() {
+		if (_bugInjectionOn) {
+			throw new UnsupportedOperationException("Injected bug for getRented()");
+		}
+		return _rented;
+	}
 
-    
-    
+	/**
+	 * Returns string version of this cat, in form: "ID *id_num*. *name*" Example
+	 * for cat of ID 1, name Jennyanydots: "ID 1. Jennyanydots"
+	 * 
+	 * @return String string version of cat
+	 */
+
+	public String toString() {
+		if (_bugInjectionOn) {
+			throw new UnsupportedOperationException("Injected bug for toString()");
+		}
+		return "ID " + _id + ". " + _name;
+	}
+
 }
--- exercises/2/RentACatTest.java
+++ exercises/2/RentACatTest.java
@@ -8,25 +8,29 @@ import org.mockito.*;
 public class RentACatTest {
 
 	/**
-     * The test fixture for this JUnit test.
-     * Test fixture: a fixed state of a set of objects used as a baseline for running tests.
-     * A test fixture is the JUnit term for a common precondition for all tests in this suite.
-     */
+	 * The test fixture for this JUnit test. Test fixture: a fixed state of a set of
+	 * objects used as a baseline for running tests. A test fixture is the JUnit
+	 * term for a common precondition for all tests in this suite.
+	 */
 
-	RentACat _r;        // Object to test
-	@Mock Cat _c1;      // First mock cat object
-	@Mock Cat _c2;      // Second mock cat object
-	@Mock Cat _c3;      // Third mock cat object
-	
+	RentACat _r; // Object to test
+	@Mock
+	Cat _c1; // First mock cat object
+	@Mock
+	Cat _c2; // Second mock cat object
+	@Mock
+	Cat _c3; // Third mock cat object
+
 	@Before
 	public void setUp() throws Exception {
 		// TODO: INITIALIZE THE TEST FIXTURE HERE.
 		// 1. CREATE A NEW RENTACAT OBJECT BEFORE EACH TEST
 		// 2. CREATE ALL MOCK CAT OBJECTS YOU WILL BE USING IN YOUR TESTS
-		// 3. PERFORM ANY STUBBING REQUIRED FOR CAT METHODS YOU WILL BE CALLING	
-		// (NOTE: THE DEFAULT BEHAVIOR FOR NON-STUBBED METHODS IS A NO-OP WITH 0 RETURN VALUE)
+		// 3. PERFORM ANY STUBBING REQUIRED FOR CAT METHODS YOU WILL BE CALLING
+		// (NOTE: THE DEFAULT BEHAVIOR FOR NON-STUBBED METHODS IS A NO-OP WITH 0 RETURN
+		// VALUE)
 	}
-    
+
 	@After
 	public void tearDown() throws Exception {
 		_r = null;
@@ -34,6 +38,7 @@ public class RentACatTest {
 		_c2 = null;
 		_c3 = null;
 	}
-	
+
 	// TODO: ADD TEST CASES FOR EACH PUBLIC METHOD IN RENTACAT EXCEPT MAIN
+
 }
